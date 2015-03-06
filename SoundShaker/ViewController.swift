8ac5eb4d152544b31d34d953057dfe2f6afb744a
Rommel Rico <rommeltj@gmail.com>
2015-03-05 21:14:06 -0800

    Finishing the app.

--- SoundShaker/ViewController.swift
+++ SoundShaker/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player:AVAudioPlayer = AVAudioPlayer()
+    var files = ["The+Clash+Spanish+Bombs", "Little+Dragon+-+Shuffle+A+Dream+[Album+Version]", "The+Hives", "Mind+Your+Manners+(Official+Audio)+-+Pearl+Jam", "Unknown+Mortal+Orchestra-+Bicycle", "Lorde+-+Royals+(US+Version)"]
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        if event.subtype == UIEventSubtype.MotionShake {
+            let randomNumber = Int(arc4random_uniform(UInt32(files.count)))
+            let fileLocation = NSString(string: NSBundle.mainBundle().pathForResource(files[randomNumber], ofType: "mp3")!)
+            var error:NSError? = nil
+            player = AVAudioPlayer(contentsOfURL: NSURL(string: fileLocation), error: &error)
+            player.play()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
